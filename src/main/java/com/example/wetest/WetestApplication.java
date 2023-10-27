7615aadbd6459e77b3c9a0a71d17e901d051b2b1
akdntmrkdjqtsp <gytjd9516@naver.com>
2023-10-27 12:21:57 +0900

    Initial commit

---
+++ src/main/java/com/example/wetest/WetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetestApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WetestApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/example/wetest/WetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetestApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
